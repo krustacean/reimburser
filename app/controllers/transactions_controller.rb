540e6b46594d164651e0d362c5bdef52c1c4d718
jshin8 <joshshin8@gmail.com>
2015-11-19 16:47:47 -0800

    added transaction views, new and show

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,19 @@
 class TransactionsController < ApplicationController
+	def new
+		@user= current_user
+		@transaction = Transaction.new
+		render :new #render new post
+	end
+
+	def show
+		@user= current_user
+   	 	@transaction = Transaction.find(params[:id])
+	   	if user_signed_in?
+			user_signed_in=true
+		else
+			user_signed_in=false
+		end
+		p params[:id]
+		render :show	
+  	end
 end
